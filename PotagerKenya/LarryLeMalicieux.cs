c888535d042027f0a9949997d8746005e316fd63
{Evan-Elbaz} <{evan.e9754@gmail.com}>
2025-05-23 15:39:34 +0200

    Larry le malicieux fini

--- PotagerKenya/LarryLeMalicieux.cs
+++ PotagerKenya/LarryLeMalicieux.cs
@@ -77,10 +77,236 @@ public class LarryLeMalicieux : Animal
             Console.WriteLine(terrain);
             Console.WriteLine("LARRY LE MALICIEUX EST PARTI EMPLI DE MALICE");
           }
-          else
-          {
-            
-          }
+              else // Si vous avez des ressources à lui donner
+              {
+                probaDisparition = rng.Next(0, 2);
+                if (probaDisparition == 0) // Larry le malicieux ne pose pas de piège et part quand on lui donne une ressource (1/2)
+                {
+                  switch (numeroTerrainLarry)
+                  {
+                    case 1:
+                      Console.WriteLine("Voulez-vous lui donner ? (mangue 🥭, baobab 🌳, sorgho 🌿)");
+                      ressourcePourLarry = Convert.ToString(Console.ReadLine());
+                      if (ressourcePourLarry == "mangue")
+                      {
+                        if (terrain.ressources[0] > 0)
+                          terrain.ressources[0]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de mangue à donner à Chuck");
+                      }
+                      else if (ressourcePourLarry == "baobab")
+                      {
+                        if (terrain.ressources[1] > 0)
+                          terrain.ressources[1]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de baobab à donner à Chuck");
+
+                      }
+                      else if (ressourcePourLarry == "sorgho")
+                      {
+                        if (terrain.ressources[2] > 0)
+                          terrain.ressources[2]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de sorgho à donner à Chuck");
+                      }
+                      break;
+                    case 2:
+                      Console.WriteLine("Voulez-vous lui donner ? (avocat 🥑, sorgho 🍆, coco 🥥)");
+                      ressourcePourLarry = Convert.ToString(Console.ReadLine());
+                      if (ressourcePourLarry == "avocat")
+                      {
+                        if (terrain.ressources[0] > 0)
+                          terrain.ressources[0]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus d'avocat à donner à Chuck");
+                      }
+                      if (ressourcePourLarry == "sorgho")
+                      {
+                        if (terrain.ressources[1] > 0)
+                          terrain.ressources[1]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de sorgho à donner à Chuck");
+                      }
+                      if (ressourcePourLarry == "coco")
+                      {
+                        if (terrain.ressources[2] > 0)
+                          terrain.ressources[2]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de sorgho à donner à Chuck");
+                      }
+                      break;
+                    case 3:
+                      Console.WriteLine("Voulez-vous lui donner ?  (lentille 🟢, blé 🌾, rose 🌹)");
+                      ressourcePourLarry = Convert.ToString(Console.ReadLine());
+                      if (ressourcePourLarry == "lentille")
+                      {
+                        if (terrain.ressources[0] > 0)
+                          terrain.ressources[0]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de lentille à donner à Chuck");
+                      }
+                      if (ressourcePourLarry == "blé")
+                      {
+                        if (terrain.ressources[1] > 0)
+                          terrain.ressources[1]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de blé à donner à Chuck");
+                      }
+                      if (ressourcePourLarry == "rose")
+                      {
+                        if (terrain.ressources[2] > 0)
+                          terrain.ressources[2]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de rose à donner à Chuck");
+                      }
+                      break;
+                    case 4:
+                      Console.WriteLine("Voulez-vous lui donner ? (ananas 🍍, tomate 🍅, palmier 🌴)");
+                      ressourcePourLarry = Convert.ToString(Console.ReadLine());
+                      if (ressourcePourLarry == "ananas")
+                      {
+                        if (terrain.ressources[0] > 0)
+                          terrain.ressources[0]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus d'ananas à donner à Chuck");
+                      }
+                      if (ressourcePourLarry == "tomate")
+                      {
+                        if (terrain.ressources[1] > 0)
+                          terrain.ressources[1]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de tomate à donner à Chuck");
+                      }
+                      if (ressourcePourLarry == "palmier")
+                      {
+                        if (terrain.ressources[2] > 0)
+                          terrain.ressources[2]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de palmier à donner à Chuck");
+                      }
+                      break;
+                  }
+                  terrain.grille[unLarry.x][unLarry.y] = "🟫";
+                  Console.WriteLine(terrain);
+                  Console.WriteLine("LARRY LE MALICIEUX A ACCEPTE VOTRE CADEAU ET EST PARTI EMPLI DE MALICE"); // Larry le malicieux part sans causer de dégâts
+                }
+                else //Larry le malicieux pose son piège et part alors même qu'on lui donne une ressource pour qu'il ne le fasse pas (1/2)
+                {
+                  switch (numeroTerrainLarry)
+                  {
+                    case 1:
+                      Console.WriteLine("Voulez-vous lui donner ? (mangue 🥭, baobab 🌳, sorgho 🌿)");
+                      ressourcePourLarry = Convert.ToString(Console.ReadLine());
+                      if (ressourcePourLarry == "mangue")
+                      {
+                        if (terrain.ressources[0] > 0)
+                          terrain.ressources[0]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de mangue à donner à Chuck");
+                      }
+                      else if (ressourcePourLarry == "baobab")
+                      {
+                        if (terrain.ressources[1] > 0)
+                          terrain.ressources[1]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de baobab à donner à Chuck");
+
+                      }
+                      else if (ressourcePourLarry == "sorgho")
+                      {
+                        if (terrain.ressources[2] > 0)
+                          terrain.ressources[2]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de sorgho à donner à Chuck");
+                      }
+                      break;
+                    case 2:
+                      Console.WriteLine("Voulez-vous lui donner ? (avocat 🥑, sorgho 🍆, coco 🥥");
+                      ressourcePourLarry = Convert.ToString(Console.ReadLine());
+                      if (ressourcePourLarry == "avocat")
+                      {
+                        if (terrain.ressources[0] > 0)
+                          terrain.ressources[0]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus d'avocat à donner à Chuck");
+                      }
+                      if (ressourcePourLarry == "sorgho")
+                      {
+                        if (terrain.ressources[1] > 0)
+                          terrain.ressources[1]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de sorgho à donner à Chuck");
+                      }
+                      if (ressourcePourLarry == "coco")
+                      {
+                        if (terrain.ressources[2] > 0)
+                          terrain.ressources[2]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de sorgho à donner à Chuck");
+                      }
+                      break;
+                    case 3:
+                      Console.WriteLine("Voulez-vous lui donner ?  (lentille 🟢, blé 🌾, rose 🌹");
+                      ressourcePourLarry = Convert.ToString(Console.ReadLine());
+                      if (ressourcePourLarry == "lentille")
+                      {
+                        if (terrain.ressources[0] > 0)
+                          terrain.ressources[0]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de lentille à donner à Chuck");
+                      }
+                      if (ressourcePourLarry == "blé")
+                      {
+                        if (terrain.ressources[1] > 0)
+                          terrain.ressources[1]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de blé à donner à Chuck");
+                      }
+                      if (ressourcePourLarry == "rose")
+                      {
+                        if (terrain.ressources[2] > 0)
+                          terrain.ressources[2]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de rose à donner à Chuck");
+                      }
+                      break;
+                    case 4:
+                      Console.WriteLine("Voulez-vous lui donner ? (ananas 🍍, tomate 🍅, palmier 🌴");
+                      ressourcePourLarry = Convert.ToString(Console.ReadLine());
+                      if (ressourcePourLarry == "ananas")
+                      {
+                        if (terrain.ressources[0] > 0)
+                          terrain.ressources[0]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus d'ananas à donner à Chuck");
+                      }
+                      if (ressourcePourLarry == "tomate")
+                      {
+                        if (terrain.ressources[1] > 0)
+                          terrain.ressources[1]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de tomate à donner à Chuck");
+                      }
+                      if (ressourcePourLarry == "palmier")
+                      {
+                        if (terrain.ressources[2] > 0)
+                          terrain.ressources[2]--;
+                        else
+                          Console.WriteLine("Vous n'avez plus de palmier à donner à Chuck");
+                      }
+                      break;
+                  }
+                  terrain.grille[unLarry.x][unLarry.y] += "⬛";
+                  Console.WriteLine(terrain);
+                  Console.WriteLine("LARRY LE MALICIEUX A DEPOSE UN PIEGE MALICIEUX (Aucune plante ne peut pousser dessus)  et a pris la plante que vous lui avez gentiment donnée (Appuyez sur ENTREE)");
+                  Console.ReadLine();
+                  terrain.grille[unLarry.x][unLarry.y] = "⬛";
+                  Console.WriteLine(terrain);
+                  Console.WriteLine("LARRY LE MALICIEUX EST PARTI EMPLI DE MALICE");
+                }
+
+              }
+            }
         }
       }
     }
@@ -88,4 +314,3 @@ public class LarryLeMalicieux : Animal
 
     
   }
-}

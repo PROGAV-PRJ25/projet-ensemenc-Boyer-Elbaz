12c536e3362469391f0473485d886e01025aa6e5
{Evan-Elbaz} <{evan.e9754@gmail.com}>
2025-05-23 17:21:46 +0200

    Fée

--- PotagerKenya/Chunk.cs
+++ PotagerKenya/Chunk.cs
@@ -13,7 +13,7 @@ public class Chunk : Animal
     int x_random;
     int y_random;
     Random rng = new Random();
-    int probaApparition = rng.Next(0,4); // Chunk le gourmand 🐿️ apparaît une fois sur 4 pour s'emparer de nos récoltes
+    int probaApparition = rng.Next(0,4); // Chunk le gourmand 🐿️ apparaît 1 fois sur 4 pour s'emparer de nos récoltes
     if (probaApparition == 0)
     {
       int numeroTerrainChunk = rng.Next(1, 5);
--- PotagerKenya/Fee.cs
+++ PotagerKenya/Fee.cs
@@ -1,12 +1,206 @@
 public class Fee : Animal
 {
-  public Fee() : base("Fée")
-  {
-
-  }
+  public Fee() : base("Fée") { }
 
   public override void ApparitionAnimal(Monde monde)
   {
-    
+    int planteChoisieParLaFee;
+    Random rng = new Random();
+    int probaApparition = rng.Next(0, 2); // Larry le malicieux apparaît 1 fois sur 3
+    if (probaApparition == 0)
+    {
+
+      int numeroTerrainFee = rng.Next(1, 5);
+      foreach (Terrain terrain in monde.terrainsDuMonde)
+      {
+        if (terrain.numeroTerrain == numeroTerrainFee)
+        {
+          int x_random, y_random;
+          do
+          {
+            x_random = rng.Next(0, 7);
+            y_random = rng.Next(0, 7);
+          }
+          while (terrain.grille[x_random][y_random] != "🟫");
+
+          this.x = x_random;
+          this.y = y_random;
+
+          terrain.grille[this.x][this.y] = "🧚";
+
+          Console.WriteLine(terrain);
+          Console.WriteLine($"UNE FEE EST APPARUE SUR LE TERRAIN {numeroTerrainFee}! POUR VOUS AIDER ! (Appuyer sur ENTREE)");
+          Console.ReadLine();
+        }
+
+        switch (numeroTerrainFee)
+        {
+
+          case 1: // Si la fée est apparue sur le terrain 1
+            planteChoisieParLaFee = rng.Next(1, 4);
+            if (planteChoisieParLaFee == 1)
+            {
+              Mangue nouvellePlante = new Mangue();
+              terrain.plantes.Add(nouvellePlante);
+              nouvellePlante.x = this.x;
+              nouvellePlante.y = this.x;
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] += "🥭";
+              Console.WriteLine(terrain);
+              Console.WriteLine("La fée a fait apparaître une mangue");
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] = "🥭";
+              Console.WriteLine(terrain);
+            }
+            else if (planteChoisieParLaFee == 2)
+            {
+              Baobab nouvellePlante = new Baobab();
+              terrain.plantes.Add(nouvellePlante);
+              nouvellePlante.x = this.x;
+              nouvellePlante.y = this.x;
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] += "🌳";
+              Console.WriteLine(terrain);
+              Console.WriteLine("La fée a fait apparaître un baobab");
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] = "🌳";
+              Console.WriteLine(terrain);
+            }
+            else if (planteChoisieParLaFee == 3)
+            {
+              Sorgho nouvellePlante = new Sorgho();
+              terrain.plantes.Add(nouvellePlante);
+              nouvellePlante.x = this.x;
+              nouvellePlante.y = this.x;
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] += "🌿";
+              Console.WriteLine(terrain);
+              Console.WriteLine("La fée a fait apparaître un sorgho");
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] = "🌿";
+              Console.WriteLine(terrain);
+            }
+            break;
+          case 2: // Si la fée est apparue sur le terrain 2
+            planteChoisieParLaFee = rng.Next(1, 4);
+            if (planteChoisieParLaFee == 1)
+            {
+              Avocat nouvellePlante = new Avocat();
+              terrain.plantes.Add(nouvellePlante);
+              nouvellePlante.x = this.x;
+              nouvellePlante.y = this.x;
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] += "🥑";
+              Console.WriteLine(terrain);
+              Console.WriteLine("La fée a fait apparaître un avocat");
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] = "🥑";
+              Console.WriteLine(terrain);
+            }
+            else if (planteChoisieParLaFee == 2)
+            {
+              Safou nouvellePlante = new Safou();
+              terrain.plantes.Add(nouvellePlante);
+              nouvellePlante.x = this.x;
+              nouvellePlante.y = this.x;
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] += "🍆";
+              Console.WriteLine(terrain);
+              Console.WriteLine("La fée a fait apparaître un safou");
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] = "🍆";
+              Console.WriteLine(terrain);
+            }
+            else if (planteChoisieParLaFee == 3)
+            {
+              Cocotier nouvellePlante = new Cocotier();
+              terrain.plantes.Add(nouvellePlante);
+              nouvellePlante.x = this.x;
+              nouvellePlante.y = this.x;
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] += "🥥";
+              Console.WriteLine(terrain);
+              Console.WriteLine("La fée a fait apparaître un cocotier");
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] = "🥥";
+              Console.WriteLine(terrain);
+            }
+            break;
+          case 3: // Si la fée est apparue sur le terrain 3
+            planteChoisieParLaFee = rng.Next(1, 4);
+            if (planteChoisieParLaFee == 1)
+            {
+              Lentille nouvellePlante = new Lentille();
+              terrain.plantes.Add(nouvellePlante);
+              nouvellePlante.x = this.x;
+              nouvellePlante.y = this.x;
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] += "🟢";
+              Console.WriteLine(terrain);
+              Console.WriteLine("La fée a fait apparaître une lentille");
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] = "🟢";
+              Console.WriteLine(terrain);
+            }
+            else if (planteChoisieParLaFee == 2)
+            {
+              Ble nouvellePlante = new Ble();
+              terrain.plantes.Add(nouvellePlante);
+              nouvellePlante.x = this.x;
+              nouvellePlante.y = this.x;
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] += "🌾";
+              Console.WriteLine(terrain);
+              Console.WriteLine("La fée a fait apparaître du blé");
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] = "🌾";
+              Console.WriteLine(terrain);
+            }
+            else if (planteChoisieParLaFee == 3)
+            {
+              Rose nouvellePlante = new Rose();
+              terrain.plantes.Add(nouvellePlante);
+              nouvellePlante.x = this.x;
+              nouvellePlante.y = this.x;
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] += "🌹";
+              Console.WriteLine(terrain);
+              Console.WriteLine("La fée a fait apparaître une rose");
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] = "🌹";
+              Console.WriteLine(terrain);
+            }
+            break;
+          case 4: // Si la fée est apparue sur le terrain 4
+            planteChoisieParLaFee = rng.Next(1, 4);
+            if (planteChoisieParLaFee == 1)
+            {
+              Ananas nouvellePlante = new Ananas();
+              terrain.plantes.Add(nouvellePlante);
+              nouvellePlante.x = this.x;
+              nouvellePlante.y = this.x;
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] += "🍍";
+              Console.WriteLine(terrain);
+              Console.WriteLine("La fée a fait apparaître un ananas");
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] = "🍍";
+              Console.WriteLine(terrain);
+            }
+            else if (planteChoisieParLaFee == 2)
+            {
+              Tomate nouvellePlante = new Tomate();
+              terrain.plantes.Add(nouvellePlante);
+              nouvellePlante.x = this.x;
+              nouvellePlante.y = this.x;
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] += "🍅";
+              Console.WriteLine(terrain);
+              Console.WriteLine("La fée a fait apparaître une tomate");
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] = "🍅";
+              Console.WriteLine(terrain);
+            }
+            else if (planteChoisieParLaFee == 3)
+            {
+              PalmierAHuile nouvellePlante = new PalmierAHuile();
+              terrain.plantes.Add(nouvellePlante);
+              nouvellePlante.x = this.x;
+              nouvellePlante.y = this.x;
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] += "🌴";
+              Console.WriteLine(terrain);
+              Console.WriteLine("La fée a fait apparaître un palmier");
+              terrain.grille[nouvellePlante.x][nouvellePlante.y] = "🌴";
+              Console.WriteLine(terrain);
+            }
+            break;
+
+
+        }
+        Console.WriteLine("La fée a disparu comme elle est arrivée");
+        
+
+        
+      }
+    }
+
   }
 }
